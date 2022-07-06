5609596f76e13197d5476c02512996699e90ba6c
Taylor Monacelli <taylormonacelli@gmail.com>
2022-07-06 09:07:26 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+https://minikube.sigs.k8s.io/docs/tutorials/nginx_tcp_udp_ingress/
---
+++ main.sh
@@ -0,0 +1,12 @@
+minikube delete
+minikube start
+kubectl get configmaps --all-namespaces
+minikube addons enable ingress
+kubectl get configmaps --all-namespaces
+kubectl apply -f redis-deployment.yaml
+kubectl apply -f redis-service.yaml
+kubectl patch configmap tcp-services -n ingress-nginx --patch '{"data":{"6379":"default/redis-service:6379"}}'
+kubectl get configmap tcp-services -n ingress-nginx -o yaml
+cat ingress-nginx-controller-patch.yaml
+kubectl patch deployment ingress-nginx-controller --patch "$(cat ingress-nginx-controller-patch.yaml)" -n ingress-nginx
+telnet $(minikube ip) 6379
